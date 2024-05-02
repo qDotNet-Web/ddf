22629dcd36d0bbb26dc1b569aa5f45360eabd362
knauerleo <bloqoel@gmail.com>
2024-05-02 20:34:04 +0200

    Refactored join game button style and label in HomeComponent.vue

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -17,9 +17,9 @@
                 <div class="modal-content bg-dark text-light">
                     <div class="modal-header">
                         <h5 class="modal-title" id="playModalLabel">Möchtest du einem Spiel beitreten, oder eines erstellen?</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body space-between">
                         <button type="button" class="btn btn-modal" id="createGameButton">Erstellen</button>
                         <button type="button" class="btn btn-modal" id="joinGameButton">Beitreten</button>
                     </div>

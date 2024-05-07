294586ecff06f3a786ae17a71ac9f1c93d9b143c
Marvin Brückner <marvin@qdotnet.eu>
2024-05-07 16:35:46 +0200

    Added websockets, refactor routes

---
+++ backend/app/api/router/websocket_router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from ...core.websockets import connection_manager
+
+router = APIRouter()
+
+
+@router.websocket("/")
+async def websocket_endpoint(websocket: WebSocket, lobby_code: str):
+    await connection_manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await connection_manager.broadcast(f'Nachricht erhalten von {lobby_code}: {data}')
+    except WebSocketDisconnect:
+        connection_manager.disconnect(websocket)
+        await connection_manager.broadcast(f'Client {lobby_code} disconnected')

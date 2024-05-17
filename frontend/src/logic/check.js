78888e21d0bd6edac78d8029a4fceae5a1105dfd
knauerleo <bloqoel@gmail.com>
2024-05-17 20:31:58 +0200

    chore: Remove unused check function

--- frontend/src/logic/check.js
+++
@@ -1,9 +0,0 @@
-import Cookies from 'js-cookie';
-
-export function check() {
-    let gameOptions = Cookies.get('gameOptions');
-    if (gameOptions) {
-        return true;
-    }
-    return false;
-}

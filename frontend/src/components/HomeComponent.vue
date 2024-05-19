3a2837affb5b690c49262a10328253fee8ad85da
Philipp Spranger <phispra@gmail.com>
2024-05-19 17:31:20 +0200

    finished modal dialogs create and join

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -18,7 +18,7 @@
             <button class="btn btn-main-new fr-animate fr-move-up fr-delay-3" id="startButton" data-bs-toggle="modal"
                 data-bs-target="#createLobbyModal" @click="modalCreate_visible = true">Erstellen</button>
             <button class="btn btn-main-new fr-animate fr-move-up fr-delay-5" data-bs-toggle="modal"
-                data-bs-target="#joinLobbyModal">Beitreten</button>
+                data-bs-target="#joinLobbyModal" @click="modalJoin_visible = true">Beitreten</button>
         </div>
         <!-- Modal 2 -->
         <div class="modal fade" id="anleitungsModal" tabindex="-1" aria-labelledby="anleitungsModalLabel"
@@ -48,10 +48,16 @@
             }
         }">
             <template #container="{ closeCallback }">
-                <div id="createLobbyModal" class="flex flex-column px-5 py-5 gap-4"
-                    style="border-radius: 12px; background-color: var(--main-bg-color)">
-                    <div class="inline-flex flex-column gap-2 mb-3" style="text-align: center;">
-                        <i class="pi pi-plus text-5xl"></i>
+                <div id="createLobbyModal" class="flex flex-column px-3 py-3 gap-4"
+                    style="border-radius: 12px; background-color: var(--main-bg-color)" aria-labelledby="createLobbyModalLabel">
+                    <div class="gap-2 mb-pi" style="justify-content: center; display: flex;">
+                        <div class="iconBox" style="display: inline-flex;">
+                            <i class="pi pi-plus-circle text-5xl is-size-35 is-main-design-color"></i>
+                        </div>
+
+                    </div>
+                    <div class="inline-flex flex-column gap-2 mb-4" style="text-align: center;">
+                        <h5 class="modal-title" id="createLobbyModalLabel">Lobby erstellen</h5>
                     </div>
                     <div class="inline-flex flex-column gap-2 mb-3">
                         <FloatLabel class="ip_float">
@@ -70,8 +76,8 @@
                         <Slider name="ip_playerLives" id="ip_playerLives" v-model="ip_playerLives" class="w-full mb-3"
                             :min="1" :max="10" />
                     </div>
-                    <div class="flex align-items-center gap-3">
-                        <button type="button" class="btn btn-main-new btn-modal-new" data-bs-dismiss="modal"
+                    <div class="flex align-items-center gap-3" style="display: flex;">
+                        <button type="button" class="btn btn-main-new btn-modal-new" data-bs-dismiss="modal" 
                             @click="closeCallback">Schließen</button>
                         <button type="button" id="createLobbyButton" class="btn btn-main-new btn-modal-new"
                             @click="createLobby()">Starten</button>
@@ -80,45 +86,47 @@
             </template>
         </Dialog>
         <!-- Modal 4 JoinLobby -->
-        <div class="modal fade" id="joinLobbyModal" tabindex="-1" aria-labelledby="joinLobbyModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content bg-dark text-light">
-                    <div class="modal-header">
+        <Dialog v-model:visible="modalJoin_visible" modal :pt="{
+            root: 'border-none',
+            mask: {
+                style: 'backdrop-filter: blur(3px)'
+            }
+        }">
+            <template #container="{ closeCallback }">
+                <div id="joinLobbyModal" class="flex flex-column px-3 py-3 gap-4"
+                    style="border-radius: 12px; background-color: var(--main-bg-color)" aria-labelledby="joinLobbyModalLabel">
+                    <div class="gap-2 mb-pi" style="justify-content: center; display: flex;">
+                        <div class="iconBox" style="display: inline-flex;">
+                            <i class="pi pi-arrow-circle-right text-5xl is-size-35 is-main-design-color"></i>
+                        </div>
+
+                    </div>
+                    <div class="inline-flex flex-column gap-2 mb-4" style="text-align: center;">
                         <h5 class="modal-title" id="joinLobbyModalLabel">Lobby beitreten</h5>
                     </div>
-                    <div class="modal-body">
-                        <div class="input-text input-text-bb mb-3">
-                            <FloatLabel class="ip_float">
-                                <InputText id="ip_lobbyID" name="ip_lobbyID" v-model="ip_lobbyID"
-                                    class="w-full ip_float" />
-                                <label for="ip_lobbyID">Lobby-ID</label>
-                            </FloatLabel>
-                            <!-- <label for="ip_playerName">Lobby-ID</label>
-                                <input type="text" name="ip_lobbyID" placeholder="1B3C5D7E" maxlength="8"
-                                    v-model="lobbyId"> -->
-                        </div>
-                        <div class="input-text input-text-bb mb-3">
-                            <FloatLabel class="ip_float">
-                                <InputText id="ip_playerName" name="ip_playerName" v-model="ip_playerName"
-                                    class="w-full ip_float" maxlength="20" />
-                                <label for="ip_playerName">Username</label>
-                            </FloatLabel>
-                            <!-- <label for="ip_playerName">Dein Name</label>
-                                <input type="text" name="ip_playerName" placeholder="Schlaubischlumpf" maxlength="20"
-                                    v-model="playerName"> -->
-                        </div>
-                        <div class="modal-footer space-between">
-                            <button type="button" class="btn btn-main-new btn-modal-new"
-                                data-bs-dismiss="modal">Schließen</button>
-                            <button type="button" class="btn btn-main-new btn-modal-new"
-                                @click="joinLobby()">Beitreten</button>
-                        </div>
+                    <div class="inline-flex flex-column gap-2 mb-custom-4">
+                        <FloatLabel class="ip_float">
+                            <InputText id="ip_lobbyID" name="ip_lobbyID" v-model="ip_lobbyID" class="w-full ip_float" />
+                            <label for="ip_lobbyID">Lobby-ID</label>
+                        </FloatLabel>
                     </div>
-
+                    <div class="inline-flex flex-column gap-2 mb-4">
+                        <FloatLabel class="ip_float">
+                            <InputText id="ip_playerName" name="ip_playerName" v-model="ip_playerName"
+                                class="w-full ip_float" maxlength="20" />
+                            <label for="ip_playerName">Username</label>
+                        </FloatLabel>
+                    </div>
+                    <div class="flex align-items-center gap-3" style="display: flex;">
+                        <button type="button" class="btn btn-main-new btn-modal-new" data-bs-dismiss="modal"
+                            @click="closeCallback">Schließen</button>
+                        <button type="button" class="btn btn-main-new btn-modal-new"
+                            @click="joinLobby()">Beitreten</button>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </template>
+        </Dialog>
+
         <!-- Footer -->
     </div>
 </template>
@@ -189,6 +197,7 @@ export default {
         const lobbyType_options = ref(['Text', 'Voice']);
         const ip_lobbyID = ref(null);
         const modalCreate_visible = ref(false);
+        const modalJoin_visible = ref(false);
 
         const data = reactive({
             lobbyId: '',
@@ -280,7 +289,7 @@ export default {
             toggleLoadingScreen(false);
             router.push("/waitingLobby");
         }
-        return { createLobby, joinLobby, ip_roundLength, ip_playerName, ip_playerLives, ip_lobbyType, lobbyType_options, ip_lobbyID, modalCreate_visible }
+        return { createLobby, joinLobby, ip_roundLength, ip_playerName, ip_playerLives, ip_lobbyType, lobbyType_options, ip_lobbyID, modalCreate_visible, modalJoin_visible }
 
     },
 }
